00f89a7085cad708924253c2929bf35e2e3efede
Callum Hibbert <callum.hibbert@gmail.com>
2012-02-02 19:54:40 +0000

    Upgraded StealFocus.Build package.

---
+++ Source/packages/StealFocus.Build.1.0.20201.0/Scripts/StealFocus.Build.psm1
@@ -0,0 +1,21 @@
+﻿function Check-Prerequisites
+{
+	param(
+			[bool]$azurePublishActionRequired = $(throw '$azurePublishActionRequired is a required parameter')
+		)
+
+	if ($azurePublishActionRequired)
+	{
+		if ((Get-PSSnapin | ?{$_.Name -eq "WAPPSCmdlets"}) -eq $null)
+		{
+			Add-PSSnapin WAPPSCmdlets -erroraction SilentlyContinue
+			$SnapIn = Get-PSSnapIn WAPPSCmdlets -erroraction SilentlyContinue
+			if ($SnapIn -eq $null)
+			{
+				Write-Error "To run this script the 'Windows Azure Platform PowerShell Cmdlets ' are required."
+				Write-Error "Please download from 'http://wappowershell.codeplex.com/' and install as a PowerShell Snap-in (not as a Module)."
+				Exit
+			}
+		}
+	}
+}
